ef58357c4d6df435f53f8dfd7f09458c5abfc6c9
Olena <kryvtsovaolena@gmail.com>
2022-10-31 09:41:54 +0200

    Update App.jsx
    nanoid

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,6 @@ import Form from '../Form/Form';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container } from './App.styled';
 
-let id = nanoid();
 class App extends Component {
   state = {
     contacts: [],
@@ -14,7 +13,7 @@ class App extends Component {
     console.log(data);
 
     const contact = {
-      id,
+      id: nanoid(),
       data,
     };
     console.log(contact);
